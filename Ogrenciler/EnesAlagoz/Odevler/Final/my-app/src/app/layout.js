890ff02493b1acc357308ca103406ce95acccee9
Enes Alagöz <alagozenes13@gmail.com>
2024-07-08 00:57:06 +0300

    create

--- Ogrenciler/EnesAlagoz/Odevler/Final/my-app/src/app/layout.js
+++ Ogrenciler/EnesAlagoz/Odevler/Final/my-app/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
