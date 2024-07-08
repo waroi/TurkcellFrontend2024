70d6fbb897e9cdcc7a08aa13f34c7ba226adaac5
Onur Ardıç <onurardc@outlook.com>
2024-07-08 20:04:17 +0300

    Yorum tasarımı tamamlandı

---
+++ Ogrenciler/OnurArdic/Odevler/final/src/Components/Details/Reviews/ReviewsNav.jsx
@@ -0,0 +1,13 @@
+import '../ProductDetails.scss'
+
+const ReviewsNav = () => {
+  return (
+    <>
+      <h4 className="nav-link">ProductDetails</h4>
+      <h4 className="nav-link active">Reviews</h4>
+      <h4 className="nav-link">FAQs</h4>
+    </>
+  )
+}
+
+export default ReviewsNav
--- Ogrenciler/OnurArdic/Odevler/final/src/app/[locale]/(Sites)/details/[category]/[id]/page.jsx
+++ Ogrenciler/OnurArdic/Odevler/final/src/app/[locale]/(Sites)/details/[category]/[id]/page.jsx
@@ -5,6 +5,11 @@ renderStars
 import ProductVariants from '@/Components/Details/ProductVariants'
 import ProductDetailsİmages from '@/Components/Details/ProductDetailsİmages'
 import { renderStars } from '@/app/Hooks/renderStar'
+import CommentsCard from '@/Components/Home/HappyCustomer/CommentsCard'
+import { getComments } from '@/service/api'
+import { useTranslations } from 'next-intl'
+import ReviewsNav from '@/Components/Details/Reviews/ReviewsNav'
+import CustomButton from '@/Components/ui/CustomButton'
 async function getProductDetail(category, id) {
   const res = await fetch(`http://localhost:3000/${category}/${id}`)
 
@@ -14,13 +19,15 @@ async function getProductDetail(category, id) {
 
   return res.json()
 }
+
 const Details = async ({ params }) => {
   const { category, id } = params
   const data = await getProductDetail(category, id)
+  const comments = await getComments()
   return (
     <>
       <div className="line container"></div>
-      <section className="product-detail-page">
+      <section className="product-detail-page container">
         <div className="product-detail-wrapper container">
           <div className="product-detail-images">
             <ProductDetailsİmages data={data} />
@@ -42,6 +49,32 @@ const Details = async ({ params }) => {
             <ProductVariants data={data} />
           </div>
         </div>
+
+        <section className="reviews-wrap">
+          <div className="reviews-wrapper">
+            <div className="product-detail-nav">
+              <ReviewsNav />
+            </div>
+          </div>
+
+          <div className="sort-btn">
+            <select id="sort">
+              <option value="Latest">Latest</option>
+              <option value="New">New</option>
+            </select>
+
+            <button className="write-review-modal">Write a Review</button>
+          </div>
+          <div className="rewiews">
+            {comments.map((comment) => (
+              <CommentsCard comment={comment} />
+            ))}
+          </div>
+
+          <div className="button-wrapper">
+            <CustomButton text={'Load More Views'} />
+          </div>
+        </section>
       </section>
     </>
   )
