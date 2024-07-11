21ffde0cd0b5b61f759747e7ab2cd3235a20c09e
Onur Ardıç <onurardc@outlook.com>
2024-07-11 15:04:59 +0300

    detay sayfası tamamlandı

--- Ogrenciler/OnurArdic/Odevler/final/src/Components/Details/Reviews/CommentSection.jsx
+++ Ogrenciler/OnurArdic/Odevler/final/src/Components/Details/Reviews/CommentSection.jsx
@@ -4,30 +4,38 @@ import { useState } from 'react'
 import CommentModal from '@/Components/ui/CommentModal'
 import SortOption from './SortOption'
 import CommentsCard from '@/Components/Home/HappyCustomer/CommentsCard'
-import CustomButton from '@/Components/ui/CustomButton'
 
 const CommentSection = ({ initialComments }) => {
   const [sortedComments, setSortedComments] = useState(initialComments)
+  const [visibleComments, setVisibleComments] = useState(4) // başlangıçta görünen yorum sayısı
 
   const handleNewComment = (newComment) => {
     setSortedComments((prevComments) => [newComment, ...prevComments])
   }
 
+  const handleLoadMore = () => {
+    setVisibleComments((prevVisibleComments) => prevVisibleComments + 4)
+  }
+
   return (
-    <div>
+    <>
       <div className="sort-btn">
         <SortOption initialComments={initialComments} setSortedComments={setSortedComments} />
         <CommentModal onCommentSubmit={handleNewComment} />
       </div>
       <div className="reviews">
-        {sortedComments.map((comment) => (
+        {sortedComments.slice(0, visibleComments).map((comment) => (
           <CommentsCard key={comment.id} comment={comment} />
         ))}
       </div>
-      <div className="button-wrapper">
-        <CustomButton text={'Load More Views'} />
-      </div>
-    </div>
+      {visibleComments < sortedComments.length && (
+        <div className="button-wrapper">
+          <button className="load-more-reviews" onClick={handleLoadMore}>
+            Load More Reviews
+          </button>
+        </div>
+      )}
+    </>
   )
 }
 
